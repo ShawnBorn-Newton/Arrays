abd88e871316ed3e60cdd4b6aa3be7c43b0167c1
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-07-05 11:08:53 -0400

    practise

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,21 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            //int[] firstArray;
+            //string[] stringArray = { "one, two, three, four" };
+            //char[] nameArray = { 'n','e','w','t','o','n' };
+            //Console.WriteLine(nameArray[0]);
+            //Console.WriteLine(nameArray[3]);
+            //string[] vacationSpots = { " VA beach", "West Mountan", "Dysney Place", "Mars", "Nagshead", "Putin Bay", "Ocean City", "The Moon", "Hawaii", "Bahmamas" };
+            //Console.WriteLine(vacationSpots[0] + vacationSpots[9]);
+
+            //int[] kidsAge = { 4, 7, 9 };
+            //Console.WriteLine("David is: " + kidsAge[0] + "Clyde is: " + kidsAge[1] + "Grace is: " + kidsAge[2]);
+
+            //double[] gpas = { 2.5, 3.4, 3.5, 3.0, 2.6, 4.0, 3.2, 1.5 };
+
+            char[] firstInital = { 'I', 'H', 'M', 'R' };
+            Console.WriteLine(firstInital);
         }
     }
 }
